ec88010083c7e799080be256a7ec913adad25b85
Ejazullah <ejazullah579@gmail.com>
2020-08-02 18:38:11 +0500

    animation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -22,31 +22,31 @@ $(document).ready(() => {
 
 $('.login_btn').on('click', function () {
     $('#main-container').css({
-        "margin-top": "0px",
+        "transform": "translate(0px)",
         "transition": "1s ease-in-out",
     });
-    console.log("core:"+user_info);
+    
 })
 function hideloginform() {
     $('#main-container').css({
-        "margin-top": "-800px",
+        "transform": "translate(-1600px)",
         "transition": "1s linear"
     });
 }
 function showsignupform() {
     $('#main-container').css({
-        "margin-top": "-800px",
+        "transform": "translate(-1600px)",
         "transition": "1s linear"
     });
 
     $('.m-container').css({
-        "margin-top": "0px",
+        "transform": "translate(0px)",
         "transition": "0.8s ease-in-out"
     });
 }
 function hidesignupform() {
     $('.m-container').css({
-        "margin-top": "800px",
+        "transform": "translate(1600px)",
         "transition": "1s ease-in-out"
     });
 }
